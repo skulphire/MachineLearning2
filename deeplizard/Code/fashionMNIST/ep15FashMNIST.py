cd99d792232e24b7349036eee328abbc4e0570ba
aphire <cdkrobotics@gmail.com>
2019-03-17 20:13:56 -0400

    ep16 code

--- deeplizard/Code/fashionMNIST/ep15FashMNIST.py
+++ deeplizard/Code/fashionMNIST/ep15FashMNIST.py
@@ -5,3 +5,4 @@ import torchvision.transforms as transforms
 train_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
                                               train=True,download=True,
                                               transform=transforms.Compose(transforms.ToTensor()))
+train_loader = torch.utils.data.DataLoader(train_set,batch_size=10)
