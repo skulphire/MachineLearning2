bfcb51a3c625272112ec81ce7aff9d1846ce7e6b
aphire <cdkrobotics@gmail.com>
2019-03-18 22:57:44 -0400

    finished example modified from kaggle

--- deeplizard/Code/fashionMNIST/ep15FashMNIST.py
+++ deeplizard/Code/fashionMNIST/ep15FashMNIST.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 train_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
                                               train=True,download=True,
                                               transform=transforms.Compose(transforms.ToTensor()))
-test_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+test_set = torchvision.datasets.FashionMNIST(root='./data/test/FashionMNIST',
                                               train=False,download=True,
                                               transform=transforms.Compose(transforms.ToTensor()))
 train_loader = torch.utils.data.DataLoader(train_set,batch_size=10,shuffle=False)
