5e6b92364f377bea1372f184fb89b703a4dfc0ee
aphire <cdkrobotics@gmail.com>
2019-03-18 23:21:49 -0400

    finished example modified from kaggle

--- deeplizard/Code/fashionMNIST/ep15FashMNIST.py
+++ deeplizard/Code/fashionMNIST/ep15FashMNIST.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-import torchvision.transforms as transforms
+from torchvision import transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
