dc29f6b6c4e07bb3ca089c51cd5e701fee5a89f5
aphire <cdkrobotics@gmail.com>
2019-03-20 23:09:32 -0400

    creating our own test files

--- mytests/fashionMNIST/fmnist.py
+++ mytests/fashionMNIST/fmnist.py
@@ -7,21 +7,21 @@ from torch.utils.data import DataLoader, Dataset
 import torch.nn as nn
 import pandas as pd
 
+SHUFFLE = False
+BATCH_SIZE = 10
 
 if __name__ == '__main__':
-    train_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+    train_set = torchvision.datasets.FashionMNIST(root='./data',
                                                   train=True, download=True,
                                                   transform=transforms.Compose([transforms.ToTensor()]))
-    test_set = torchvision.datasets.FashionMNIST(root='./data/test/FashionMNIST',
+    test_set = torchvision.datasets.FashionMNIST(root='./data/test',
                                                  train=False, download=True,
                                                  transform=transforms.Compose([transforms.ToTensor()]))
-    train_loader = DataLoader(train_set, batch_size=10, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=10, shuffle=False)
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=SHUFFLE)
+    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=SHUFFLE)
 
     batch = next(iter(train_loader))
 
     images, labels = batch  # features and labels
     # images.shape = [10,1,28,28]
     # labels.shape = [10]
-    print(images.size())
-    print(labels.size())
