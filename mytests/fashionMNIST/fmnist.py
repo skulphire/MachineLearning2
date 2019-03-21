e5a9e40cb6b0e49565c2a766ae5c54b7494d7b85
aphire <cdkrobotics@gmail.com>
2019-03-21 00:57:36 -0400

    creating our own test files

--- mytests/fashionMNIST/fmnist.py
+++ mytests/fashionMNIST/fmnist.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         oldPercentage = percentage
         model = train(Train_epoch+epochIncrease)
         percentage = test(model)
-        if not (oldPercentage-percentage) < 0.3:
+        if not (oldPercentage-percentage) < 1:
             epochIncrease = epochIncrease + int((oldPercentage-percentage)*2)
             print("Old {}%".format(oldPercentage))
             print("New {}%".format(percentage))
