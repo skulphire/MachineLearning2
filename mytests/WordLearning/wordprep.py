8ff81936a5c3e88b03c2bc694fd32525009f7cae
skulphire <cdkrobotics@gmail.com>
2019-03-24 03:04:10 -0400

    test

--- mytests/WordLearning/wordprep.py
+++ mytests/WordLearning/wordprep.py
@@ -91,7 +91,8 @@ if __name__ == '__main__':
     lexicon = createlexicon()
     trainSet,testSet = createsets(lexicon)
     print(trainSet[0])
-    print(torch.tensor(trainSet)[0])
+    print("###############################")
+    print(torch.from_numpy(trainSet)[0])
     #print(len(trainX[0]))
 
     with open ('set.pickle','wb') as f:
