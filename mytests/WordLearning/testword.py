609000cd87bb72a6b9c1a22b8ba0a642fdbceec3
skulphire <cdkrobotics@gmail.com>
2019-03-24 02:10:05 -0400

    test

--- mytests/WordLearning/testword.py
+++ mytests/WordLearning/testword.py
@@ -66,10 +66,10 @@ def createsets(lexicon, test_size=0.1):
     random.shuffle(features)
     features = np.array(features)
     testing_size = int(test_size*len(features))
-    trainX = torch.from_numpy(features[:,0][:-testing_size])
-    trainY = torch.from_numpy(features[:,1][:-testing_size])
-    testX = torch.from_numpy(features[:,0][-testing_size:])
-    testY = torch.from_numpy(features[:,1][-testing_size:])
+    trainX = list(features[:,0][:-testing_size])
+    trainY = list(features[:,1][:-testing_size])
+    testX = list(features[:,0][-testing_size:])
+    testY = list(features[:,1][-testing_size:])
 
     return trainX,trainY,testX,testY
 
