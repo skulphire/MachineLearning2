93e3ab0b39ce75cf4c40fe5f3e2a21cbcd037ff4
skulphire <cdkrobotics@gmail.com>
2019-03-24 00:21:47 -0400

    test

--- mytests/WordLearning/testword.py
+++ mytests/WordLearning/testword.py
@@ -7,4 +7,20 @@ import random
 lemmatizer = WordNetLemmatizer()
 hm_lines = 10000000
 
+POS = []
+NEG = []
+
 def readfiles():
+    with open('pos-neg-sentdex/pos.txt','r') as f:
+        contents = f.readlines()
+        for line in contents:
+            POS.append(line)
+    with open('pos-neg-sentdex/neg.txt','r') as f:
+        contents = f.readlines()
+        for line in contents:
+            NEG.append(line)
+    print(POS[1])
+    print(NEG[2])
+
+if __name__ == '__main__':
+    readfiles()
